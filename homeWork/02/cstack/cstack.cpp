4e9c058a6ec1df019ffcb0a34f376021c3f00a24
Leisan Farrakhova <113044620+Leeiss@users.noreply.github.com>
2023-10-13 15:13:25 +0300

    Update cstack.cpp

--- homeWork/02/cstack/cstack.cpp
+++ homeWork/02/cstack/cstack.cpp
@@ -1,30 +1,87 @@
-#include "cstack.hpp"
+#pragma once
+#include <cstdint>
+#include <cstddef>
+
+/*
+Необходимо реализовать класс CStack - стэк элементов uint32_t с помощью односвязного списка (node)
+
+пример работы стандартной реализации методов
+https://en.cppreference.com/w/cpp/container/stack
+
+Особенности вашей реализации в том, что
+1) если в стеке элементов нет, то метод pop ничего не делает(не выбрасывает ошибки)
+2) логику деструктора (освободить все использованные ресурсы) реализовать в функции clear()!
+
+Добавить самостоятельно и реализовать для класса CStack операторы:
+1) "*=" - умножает каждый все элементы стека(именно поле info в node) на число
+    Пример:
+        Cstack c;
+        c.push(1);
+        c.push(2);
+        c.push(3);
+        c*=3;
+    Итог:   
+        Элементы в стеке c=={9,6,3}
+2) "=" - копирует стек в другой объект стека без уничтожения
+    Пример:
+        Cstack c,c1;
+        c.push(1);
+        c.push(2);
+        c.push(3);
+        c1 = c;
+    Итог:   
+        Элементы в стеке c1=={3,2,1}
+3) "[]" - возвращает  элемент(именно поле info в node) стека под определенным номером.
+    Пример:
+        Cstack c;
+        c.push(1);
+        c.push(2);
+        c.push(3);
+        uint32_t a = c[1];
+    Итог: 
+        a==2
+
+Реализовать operator< для структуры node
+    Пример:
+        Node n,n1;
+        n.info = 1;
+        n1.info = 2;
+        if (n1 < n2)
+            std::cout << "Congratulations!";
+        else
+            std::cout << "Commiserations!";
+    Итог: 
+        Вывод Congratulations!
 
 
-CStack::CStack(){}
+10 баллов
+Перед сдачей необходимо создать pull reequest(назвать ветку своей фамилией), чтобы пройти тесты.
 
-CStack::~CStack(){
-    //destroy me!)
-    clear();
-}
+Дата последней сдачи 13.10.23
+Следующие домашки будут чаще!
+*/
 
-void CStack::pop(){
+struct Node
+{
+    uint32_t info;
+    Node* next = nullptr;
+};
 
-}
+class CStack {
+private:
+    Node* ptop = nullptr;
+    bool cleared = false;
 
-void CStack::clear(){
-
-}
-
-void CStack::push(uint32_t val){
-
-
-}
-
-uint32_t CStack::top(){
-    return 0;
-}
-
-bool CStack::empty(){
-    return ptop==nullptr;
-}
+public:
+    CStack();
+    ~CStack(); // Clears all memory
+    void pop(); // Removes the top element
+    void push(uint32_t val); // Inserts element at the top
+    uint32_t top(); // Accesses the top element
+    bool empty(); // Checks whether the underlying container is empty
+    void operator*=(uint32_t multiplier);
+    CStack& operator=(const CStack& other);
+    uint32_t operator[](size_t index) const;
+    bool operator<(const CStack& other) const;
+    void clear(); 
+};
