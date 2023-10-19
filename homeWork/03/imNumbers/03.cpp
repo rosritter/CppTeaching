298e1231a086178968a52cec045052dcfc1e414b
mosshkina <113132709+mosshkina@users.noreply.github.com>
2023-10-19 20:08:12 +0300

    Add files via upload

---
+++ homeWork/03/imNumbers/03.cpp
@@ -0,0 +1,57 @@
+﻿// 03.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "ImNumber.hpp"
+#include <iostream>
+#include "ImNumber.cpp"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    ImNumber a(2, 3);
+    ImNumber b(4, 5);
+
+    ImNumber sum = a + b;
+    std::cout << "a + b = " << sum << std::endl;
+
+    ImNumber diff = a - b;
+    std::cout << "a - b = " << diff << std::endl;
+
+    ImNumber pr = a * b;
+    std::cout << "a * b = " << pr << std::endl;
+
+    ImNumber dell = a / b;
+    std::cout << "a / b = " << dell << std::endl;
+
+    a += b;
+    std::cout << "a += b: a = " << a << std::endl;
+
+    a -= b;
+    std::cout << "a -= b: a = " << a << std::endl;
+
+    a *= b;
+    std::cout << "a *= b: a = " << a << std::endl;
+
+    a /= b;
+    std::cout << "a /= b: a = " << a << std::endl;
+
+    ImNumber sopr= !a;
+    std::cout << "Сопряженное: " << sopr<< std::endl;
+
+    double modulus = a();
+    std::cout << "Модуль: " << modulus << std::endl;
+
+    if (a == b)
+        std::cout << "a = b" << std::endl;
+    else
+        std::cout << "a != b" << std::endl;
+
+    if (a < b)
+        std::cout << "a меньше b" << std::endl;
+    else
+        std::cout << "a не меньше b" << std::endl;
+
+    double angle = a.phi();
+    std::cout << "наименьший положительный угол покательной формы комплекного числа: " << angle << std::endl;
+
+    return 0;
+}
+
